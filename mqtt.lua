bd3e7dfe9470c23bbbbe0e0a1cc21f26725cd635
pcherstvov <p.cherstvov@rotek.ru>
2018-09-17 20:02:24 +0300

    New mqtt module usings callbacks

---
+++ mqtt.lua
@@ -0,0 +1,146 @@
+local SERVICE_PERIOD = 60 * 1000
+local MQTT_KEEPALIVE = 60
+local service_tmr = tmr.create()
+local online = false
+
+
+local m_dis = {}
+
+--Initialize MQTT client with keepalive timer
+if m == nil then
+    m = mqtt.Client(MQTT_CLIENTID, MQTT_KEEPALIVE, MQTT_USERNAME, MQTT_PASSWORD) 
+else
+    m:close()
+end
+
+--Set LWT
+m:lwt('/lwt/' .. MQTT_CLIENTID, "died", 0, 0)
+
+--Publish service data: uptime, IP, rssi
+function service_pub()
+    LedBlink(50)
+    time = tmr.time()
+    dd = time / (3600 * 24)
+    hh = (time / 3600) % 24
+    mm = (time / 60) % 60
+    local str = string.format("%dd %dh %dm", dd, hh, mm)
+    m:publish("/"..MQTT_CLIENTID.."/state/uptime", str, 0, 1, nil)
+    ip = wifi.sta.getip()
+    if ip == nil then
+        ip = "unknown"
+    end
+    rssi = wifi.sta.getrssi()
+    if rssi == nil then
+        rssi = "unknown"
+    end
+    m:publish("/"..MQTT_CLIENTID.."/state/ip", ip, 0, 1, nil)
+    m:publish("/"..MQTT_CLIENTID.."/state/rssi", rssi, 0, 1, nil)
+end
+
+--When client connects, print status message and subscribe to cmd topic
+function handle_mqtt_connect(m)
+    --Set connection status flag
+    online = true
+
+    -- Serial status message
+    print("MQTT: " .. MQTT_CLIENTID .. " connected to broker " 
+        .. MQTT_HOST .. ":" .. MQTT_PORT)
+
+    -- Subscribe to the topic where the ESP8266 will get commands from
+    m:subscribe(MQTT_MAINTOPIC .. '/cmd/#', 0, function (m)
+        print('MQTT: subscribed to ' .. MQTT_MAINTOPIC) 
+    end)
+
+    --Publish service data periodicaly
+    service_pub()
+    tmr.alarm(service_tmr, SERVICE_PERIOD, tmr.ALARM_AUTO, service_pub)
+    tmr.start(service_tmr)
+end
+
+--When client disconnects, print a message and list space left on stack
+m:on("offline", function(m)
+    --Clear connection status flag
+    online = false
+
+    --Try to reconnect
+    print ("\n\nDisconnected from broker")
+    print("Heap: ", node.heap())
+    tmr.stop(service_tmr)
+    do_mqtt_connect()
+end)
+
+--Interpret the command
+m:on("message", function(m,t,pl)
+    print("PAYLOAD: ", pl)
+    print("TOPIC: ", t)
+    
+    --Run command handler
+    if pl~=nil and m_dis[t] then
+        m_dis[t](m,pl)
+    end
+end)
+
+--MQTT error handler
+function handle_mqtt_error(client, reason)
+    LedFlicker(50, 200, 5)
+    tmr.create():alarm(2 * 1000, tmr.ALARM_SINGLE, do_mqtt_connect)
+end
+
+--MQTT connect handler
+function do_mqtt_connect()
+  print("Connecting to broker " .. MQTT_HOST ..  "...")
+  m:connect(MQTT_HOST, MQTT_PORT, 0, 0, handle_mqtt_connect, handle_mqtt_error)
+end
+
+function debounce(func)
+    local last = 0
+
+    return function (...)
+        local now = tmr.now()
+        if now - last < BUTTON_DEBOUNCE then return end
+
+        last = now
+        return func(...)
+    end
+end
+
+local function switch_power(m, pl)
+	if pl == "ON" or pl == "1" then
+		gpio.write(GPIO_SWITCH, gpio.HIGH)
+		print("MQTT : plug ON for ", MQTT_CLIENTID)
+	elseif pl == "OFF" or pl == "0" then
+		gpio.write(GPIO_SWITCH, gpio.LOW)
+		print("MQTT : plug OFF for ", MQTT_CLIENTID)
+	end
+end
+
+local function toggle_power()
+    local msg
+    local status = "offline"
+	if gpio.read(GPIO_SWITCH) == gpio.HIGH then
+		gpio.write(GPIO_SWITCH, gpio.LOW)
+        msg = "OFF"
+	else
+		gpio.write(GPIO_SWITCH, gpio.HIGH)
+        msg = "ON"
+    end
+    if online then
+        m:publish(MQTT_MAINTOPIC .. '/state/power', msg, 0, 1)
+        LedBlink(100)
+        status = "online"
+    end
+    local str = string.format("MQTT (%s): %s", status, msg)
+    print(str)
+end
+
+
+--Init GPIOs
+gpio.mode(GPIO_SWITCH, gpio.OUTPUT)
+gpio.mode(GPIO_BUTTON, gpio.INT)
+gpio.trig(GPIO_BUTTON, 'down', debounce(toggle_power))
+
+--Assign MQTT handlers
+m_dis[MQTT_MAINTOPIC .. '/cmd/power'] = switch_power
+
+-- Connect to the broker
+do_mqtt_connect()
